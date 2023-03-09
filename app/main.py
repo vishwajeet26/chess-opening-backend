3be5808363c74cf18973de0f1f2c69331dce187d
Vishwajeet singh <vishwajeetsingh262005@gmail.com>
2023-03-09 15:23:43 +0530

    alembic support added

--- app/main.py
+++ app/main.py
@@ -1,9 +1,5 @@
 from fastapi import FastAPI
 import app.models as models, app.database as database
-# import sys
-# sys.path.insert(1, 'routers') 
-# import openings
-# import routers.openings
 from .routers import openings
 
 app = FastAPI()
